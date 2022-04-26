e2ed763cf179456d6e585ecefdd0a925a36b483d
TimHsu@M1PRO-20220208 <maochindada@gmail.com>
2022-04-26 16:36:11 +0800

    feat(color): fix asia color

--- lib/src/theme/color_palette.dart
+++ lib/src/theme/color_palette.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 class DarkColorPalette {
   static const grayColor = Color(0xFF848E9C);
   static const background = Color(0xFF191B20);
-  static const primaryGreen = Color(0xFF26A69A);
-  static const secondaryGreen = Color(0xFF005940);
-  static const primaryRed = Color(0xFFEF5350);
-  static const secondaryRed = Color(0xFF82122B);
+  static const primaryRed = Color(0xFF26A69A);
+  static const secondaryRed = Color(0xFF005940);
+  static const primaryGreen = Color(0xFFEF5350);
+  static const secondaryGreen = Color(0xFF82122B);
   static const hoverIndicatorBackgroundColor = Color(0xFF4C525E);
   static const digalogColor = Color(0xFF23282D);
   static const lightGold = Color(0xFF494537);
